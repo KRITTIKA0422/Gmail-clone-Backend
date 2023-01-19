081feb265356cdb76f8c5bf75c83f190db6608f7
Guvi-Assignment-day-21 <krittika489podder@gmail.com>
2023-01-19 16:28:41 +0530

    solving for cors error

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 app.use(express.json());
 const MONGO_URL = process.env.MONGO_URL;
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+ })
 app.use(cors());
 
 async function createConnection() {
