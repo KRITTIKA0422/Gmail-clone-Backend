62e8c1a3d62a0cc3168eb7b120ee619a85c1c907
Guvi-Assignment-day-21 <krittika489podder@gmail.com>
2023-01-19 17:01:53 +0530

    Added netlify url

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 4000;
 app.use(express.json());
 const MONGO_URL = process.env.MONGO_URL;
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "https://taupe-yeot-a9dff8.netlify.app");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
  })
